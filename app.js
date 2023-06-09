6363c36219da15fb4cf91fd033c628b3c4403ca6
Leonardo-Sabino <sabinoleonard731@gmail.com>
2023-06-09 01:25:22 -0100

    2nd commit

--- app.js
+++ app.js
@@ -85,8 +85,6 @@ app.put('/events/:id', async (req, res) => {
   }
 });
 
-// ...
-
 // Route to add a new event with rating, reviews, and price
 app.post('/events', async (req, res) => {
   const newEvent = {
@@ -130,9 +128,6 @@ app.post('/events', async (req, res) => {
   }
 });
 
-// ...
-
-
 // Route to delete an event by ID
 app.delete('/events/:id', async (req, res) => {
   const eventId = req.params.id;
@@ -150,6 +145,98 @@ app.delete('/events/:id', async (req, res) => {
     client.release();
   } catch (error) {
     console.error('Error deleting event:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Function to delete expired events
+async function deleteExpiredEvents() {
+  try {
+    const client = await pool.connect();
+
+    // Get the current date and time
+    const currentDate = new Date().toISOString();
+
+    // Delete events where the eventDate is less than the current date
+    const result = await client.query('DELETE FROM events WHERE eventdate < $1', [currentDate]);
+
+    console.log(`Deleted ${result.rowCount} expired event(s)`);
+
+    client.release();
+  } catch (error) {
+    console.error('Error deleting expired events:', error);
+  }
+}
+// Schedule a task to delete expired events periodically (e.g., once per day)
+setInterval(deleteExpiredEvents, 24 * 60 * 60 * 1000); // Run every 24 hours
+
+
+//nightClubs
+
+// Route to get all nigthClubs
+app.get('/nightclubs', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM nightclubs');
+    const events = result.rows;
+    client.release();
+    res.json(events);
+  } catch (error) {
+    console.error('Error fetching events:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Route to get a single event by ID
+app.get('/nightclubs/:id', async (req, res) => {
+  const nightclubId = req.params.id;
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM nightclubs WHERE id = $1', [nightclubId]);
+    const nightclub = result.rows[0];
+    client.release();
+    if (nightclub) {
+      res.json(nightclub);
+    } else {
+      res.status(404).json({ error: 'Nightclub not found' });
+    }
+  } catch (error) {
+    console.error('Error fetching nightclub:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Route to add a new nightclub
+app.post('/nightclubs', async (req, res) => {
+  const newNightclub = {
+    id: uuidv4(),
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    name: req.body.name,
+    description: req.body.description,
+    image: req.body.image,
+    rating: req.body.rating,
+    reviews: req.body.reviews
+  };
+
+  try {
+    const client = await pool.connect();
+    await client.query('INSERT INTO nightclubs (id, latitude, longitude, name, description, image, rating, reviews) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [
+      newNightclub.id,
+      newNightclub.latitude,
+      newNightclub.longitude,
+      newNightclub.name,
+      newNightclub.description,
+      newNightclub.image,
+      newNightclub.rating,
+      newNightclub.reviews
+    ]);
+
+    client.release();
+
+    res.json({ message: 'Nightclub added successfully!', nightclub: newNightclub });
+  } catch (error) {
+    console.error('Error adding nightclub:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
